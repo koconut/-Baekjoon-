f589561774fedf2c867df8d06a26a2bec5a7dab2
DODAO <yjsuk0818@naver.com>
2025-06-09 16:26:12 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T = 0;
+	int a, b;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+
+		cout << a + b << "\n";
+	}
+
+
+	return 0;
+} 
