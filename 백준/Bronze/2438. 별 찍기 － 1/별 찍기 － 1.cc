4558a29c016636009a243c82426929d5d7e7ac1b
DODAO <yjsuk0818@naver.com>
2025-06-09 16:34:49 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+
+	cin >> N;
+
+	for (int i = 1; i < N + 1; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+} 
