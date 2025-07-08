229848ecadc927718c9bc53d603032a5d933dc50
DODAO <yjsuk0818@naver.com>
2025-07-08 20:57:38 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, K, ans = 0;
+int s[2][7] = {};	// 성별/반별 학생 수를 저장하는 배열
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> K;	// 학생 수 저장
+
+	for (int i = 0; i < N; ++i) {	// 학생 저장
+		int a, b;
+		cin >> a >> b;
+		s[a][b]++;
+	}
+
+	// 필요한 방의 개수 계산
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 1; j < 7; ++j) {
+			// 배정에 필요한 만큼 방의 개수 추가
+			ans += s[i][j] / K;
+			// 학생이 남을 경우, 하나의 방이 더 필요하므로 방의 개수 추가
+			if (s[i][j] % K) ++ans;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
