78b7e0ee55ed55714cde7e4dff0e2154dc77ca92
DODAO <yjsuk0818@naver.com>
2025-07-29 15:31:52 +0900

    [Silver II] Title: 스택 수열, Time: 8 ms, Memory: 2896 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	stack<int> S;
+	int cnt = 1;
+	string ans;
+
+	while (N--) {
+		int t;
+		cin >> t;
+
+		while (cnt <= t) {
+			S.push(cnt++);
+			ans += "+\n";
+		}
+
+		if (S.top() != t) {
+			cout << "NO\n";
+			return 0;
+		}
+
+		S.pop();
+		ans += "-\n";
+	}
+
+	cout << ans;
+}
